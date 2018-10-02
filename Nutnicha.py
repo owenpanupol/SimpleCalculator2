78a3be559d5677095c31fa9fd64fe7d22f3e1ab4
nicha1362 <suyatp@gmail.com>
2018-10-02 15:42:50 +0700

    เพิ่ม

---
+++ Nutnicha.py
@@ -0,0 +1,9 @@
+print("Hello SimpleCalculator");
+print("poramet saelee")
+print("A")
+print("sattawat115")
+print("james007")
+print("waraporn113")
+print("Adisorn103")
+print("armsayamrat117")
+print("nutnicha")
