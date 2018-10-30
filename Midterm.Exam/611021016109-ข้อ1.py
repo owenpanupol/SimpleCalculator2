b8a56e49f05be1cfa89f68d5db069dee9c10a9be
narathip senasee <narathipsenasee19@gmail.com>
2018-10-30 16:13:09 +0700

    Signed-off-by: narathip senasee <narathipsenasee19@gmail.com>

---
+++ Midterm.Exam/611021016109-ข้อ1.py
@@ -0,0 +1,9 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> r = float (input("R = "))
+R = 7
+>>> area = math.pi*2*r
+>>> print (area)
+43.982297150257104
+>>> 
---
+++ Midterm.Exam/61102106109--ข้อ2.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> base = float(input("ความยาวของฐาน : " ))
+ความยาวของฐาน : 10
+>>> height = float (input("ความสูง : " ))
+ความสูง : 15
+>>> area = 0.5 * base * height
+>>> print(area)
+75.0
+>>> 
