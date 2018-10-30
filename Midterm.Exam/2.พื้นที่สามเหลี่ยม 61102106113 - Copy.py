69dd873ac03597fed0b15f26d216342ae21a9bd9
warapornfern <warapora280443@gmail.com>
2018-10-30 16:17:21 +0700

    Signed-off-by: warapornfern <warapora280443@gmail.com>

---
+++ Midterm.Exam/2.พื้นที่สามเหลี่ยม 61102106113 - Copy.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Users/Admin/Downloads/113/2.waraporn61102106113.py =======
+Enter base:9
+Enter heinght:8
+Area= 36.00
+>>> 
---
+++ Midterm.Exam/2.พื้นที่สามเหลี่ยม waraporn61102106113 - Copy.py
@@ -0,0 +1,4 @@
+base=float(input("Enter base:"))
+height=float(input("Enter heinght:"))
+area=0.5* base * height
+print("Area= %.2f" % area)
