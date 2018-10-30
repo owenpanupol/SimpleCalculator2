98921e52a71ddb853ea1ac6acf00d288aab73cff
Eakrin.ng61@snru.ac.th <Eakrin.ng61@snru.ac.th>
2018-10-30 23:00:06 +0700

    ส่งข้อสอบby เอกรินทร์ เงินพลับพลา

---
+++ Midterm.Exam/61102106111/การหาพื้นที่วงกลม.py
@@ -0,0 +1,4 @@
+import math
+r = float(input("R = "))
+area = math.pi*r**2
+print(area)
---
+++ Midterm.Exam/61102106111/การหาพื้นที่สามเหลี่ยม.py
@@ -0,0 +1,5 @@
+import math
+base = float(input("กรอกตัวเลขความยาวของฐาน : "))
+height = float(input("กรอกตัวเลขความสูง : "))
+area = 0.5 * base * height
+print(area)
