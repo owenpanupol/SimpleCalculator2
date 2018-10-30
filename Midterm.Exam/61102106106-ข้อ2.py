506cdf31ede9a5fef0e2735578e51acd8410cbec
narathip senasee <narathipsenasee19@gmail.com>
2018-10-30 16:15:23 +0700

    Signed-off-by: narathip senasee <narathipsenasee19@gmail.com>

---
+++ Midterm.Exam/61102106106-ข้อ2.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> base = float(input("ความยาวของฐาน : " ))
+ความยาวของฐาน : 12
+>>> height = float (input("ความสูง : " ))
+ความสูง : 18
+>>> area = 0.5 * base * height
+>>> print(area)
+108.0
+>>> 
