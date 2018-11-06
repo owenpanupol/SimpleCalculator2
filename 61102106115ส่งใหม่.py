b11d9ef002b5de1f74d1ed75c0b813c00018b7b9
sattawat115 <43565346+sattawat115@users.noreply.github.com>
2018-11-06 13:15:17 +0700

    Add files via upload

---
+++ 61102106115ส่งใหม่.py
@@ -0,0 +1,23 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/Admin/Desktop/61102106115ส่งใหม่.py ===========
+find the geometry area : 
+push1 find a circle area
+push2 find a triangle area
+exit
+==================&menu&===================
+number : 1
+find a circle area
+cost number = 34
+circle area =  3629.84
+==================&menu&===================
+number : 2
+find a triangle area
+Please enter a length value67
+Please enter a height value99
+triangle area = 3316.50
+==================&menu&===================
+number : 3
+thank you
+>>> 
